fa7083f9e06c25f06f1ebc7a93158fb7f5e12460
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-17 16:53:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 
   def remove_strawberry(contacts)
     contacts["Freddy Mercury"].each do |charcter, value |
-  
+
         if charcter == :favorite_ice_cream_flavors
           binding.pry
           value.delete_if  {|flavor|  flavor == "strawberry"}
